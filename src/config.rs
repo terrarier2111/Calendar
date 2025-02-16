0e5a55410bd5521f13b52ca0d579cf813ee506de
terrarier2111 <58695553+terrarier2111@users.noreply.github.com>
2025-02-16 13:59:29 +0100

    Iced (#2)

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,16 @@
 use std::{
+    borrow::Cow,
     fs,
     hash::{Hash, Hasher},
 };
 
 use fnv::FnvHasher;
 use serde::{Deserialize, Serialize};
+use tokio::runtime::Runtime;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Config {
-    calendars: Vec<Calendar>,
+    pub calendars: Vec<Calendar>,
 }
 
 impl Config {
@@ -16,12 +18,26 @@ impl Config {
         let mut path = dirs::config_dir().unwrap();
         path.push("HCal");
         let dir_path = path.clone();
-        path.set_file_name("config.json");
+        path.push("config.json");
         if !dir_path.exists() {
             fs::create_dir_all(dir_path).unwrap();
             fs::write(
                 &path,
-                serde_json::to_string_pretty(&Config { calendars: vec![] }).unwrap(),
+                serde_json::to_string_pretty(&Config {
+                    calendars: vec![Calendar {
+                        src: CalendarSrc::Web {
+                            src: "".to_string(),
+                        },
+                        name: "test".to_string(),
+                        color: ConfigColor {
+                            r: 255,
+                            g: 0,
+                            b: 0,
+                            a: 255,
+                        },
+                    }],
+                })
+                .unwrap(),
             )
             .unwrap();
         }
@@ -74,25 +90,65 @@ pub fn store_last_update(src: &str, last_update: u128) {
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Calendar {
     src: CalendarSrc,
-    name: String,
+    pub name: String,
+    pub color: ConfigColor,
+}
+
+impl Calendar {
+    pub fn get_events(&self, rt: &Runtime) -> Cow<Vec<CalEvent>> {
+        match &self.src {
+            CalendarSrc::Web { src } => {
+                println!("fetch events");
+                Cow::Owned(
+                    rt.block_on(web_ical::Calendar::new(src))
+                        .unwrap()
+                        .events
+                        .into_iter()
+                        .map(|event| CalEvent {
+                            start: event
+                                .dtstart
+                                .as_ref()
+                                .map(|dt| dt.timestamp_millis() as u64)
+                                .unwrap_or(0),
+                            finish: event
+                                .dtend
+                                .as_ref()
+                                .map(|dt| dt.timestamp_millis() as u64)
+                                .unwrap_or(0),
+                            name: event.summary.unwrap_or_default(),
+                            location: event.location.unwrap_or_default(),
+                            repeat: event.repeat.map(|rep| Repeat {
+                                freq: rep.freq,
+                                until: rep.until.map(|val| val.timestamp_millis() as u64),
+                            }),
+                        })
+                        .collect::<Vec<CalEvent>>(),
+                )
+            }
+            CalendarSrc::Local { events } => Cow::Borrowed(events),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub enum CalendarSrc {
-    Web {
-        src: String,
-    },
-    Local {
-        events: Vec<CalEvent>,
-        color: ConfigColor,
-    },
+    Web { src: String },
+    Local { events: Vec<CalEvent> },
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct CalEvent {
     pub start: u64,
     pub finish: u64,
     pub name: String,
+    pub location: String,
+    pub repeat: Option<Repeat>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Repeat {
+    pub freq: String,
+    pub until: Option<u64>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,337 @@
 mod config;
-mod ui;
-mod render;
-mod screen_sys;
 
-use std::sync::{Arc, Mutex, RwLock};
-
-use config::Config;
-use glyphon::{Attrs, AttrsOwned, Color, Shaping};
-use render::{ctx, GlyphId, GlyphInfo, Renderer, UiCtx};
-use screen_sys::Screen;
-use ui::{Container, TextBox};
-use winit::{
-    event::{Event, WindowEvent}, event_loop::{ActiveEventLoop, EventLoopBuilder}, window::WindowAttributes
+use chrono::{Datelike, Days, Timelike, Utc, Weekday};
+use iced::widget::container::bordered_box;
+use iced::widget::{
+    center, column, row, Button, Column, Container,
+    Row, Space, Text,
 };
+use iced::{Border, Color, Element, Length, Shadow, Theme};
+
+use config::{CalEvent, Config};
+use tokio::runtime::{Builder, Runtime};
 
 fn main() {
-    let event_loop = EventLoopBuilder::<()>::default().build().unwrap();
-    let window = Arc::new(
-        event_loop
-            .create_window(WindowAttributes::default().with_title("RustSpeak"))
-            .unwrap(),
-    );
-    let state =
-        Arc::new(pollster::block_on(wgpu_biolerless::StateBuilder::new().window(window.clone()).build()).unwrap());
-    let renderer = Arc::new(Renderer::new(state.clone(), &window).unwrap());
-    let config = Config::load();
-    render::init(Arc::new(UiCtx {
-        renderer: renderer.clone(),
-        window: window.clone(),
-    }));
-    let screen_sys = Arc::new(screen_sys::ScreenSystem::new());
-    screen_sys.push_screen(Box::new(DefaultScreen::new()));
-    event_loop
-        .run(move |event, control_flow| {
-            let redraw = || {
-                let models = screen_sys.tick(&Arc::new(()), &window);
-                renderer.render(models);
-            };
-            match event {
-                Event::WindowEvent { window_id, event } if window_id == window.id() => {
-                    match event {
-                        WindowEvent::Resized(size) => {
-                            if !state.resize(size) {
-                                panic!("Couldn't resize!");
-                            } else {
-                                renderer.dimensions.set(size.width, size.height);
-                            }
-                            renderer.rescale_glyphs();
-                            redraw();
-                        }
-                        WindowEvent::Moved(_) => {}
-                        WindowEvent::CloseRequested => {
-                            control_flow.exit();
-                        }
-                        WindowEvent::Destroyed => {}
-                        WindowEvent::DroppedFile(_) => {}
-                        WindowEvent::HoveredFile(_) => {}
-                        WindowEvent::HoveredFileCancelled => {}
-                        WindowEvent::Focused(_) => {}
-                        WindowEvent::KeyboardInput { event, .. } => {}
-                        WindowEvent::ModifiersChanged(_) => {}
-                        WindowEvent::CursorMoved { position, .. } => {}
-                        WindowEvent::CursorEntered { .. } => {}
-                        WindowEvent::CursorLeft { .. } => {}
-                        WindowEvent::MouseWheel { .. } => {}
-                        WindowEvent::MouseInput { button, state, .. } => {}
-                        WindowEvent::TouchpadPressure { .. } => {}
-                        WindowEvent::AxisMotion { .. } => {}
-                        WindowEvent::Touch(_) => {}
-                        WindowEvent::ScaleFactorChanged {
-                            scale_factor,
-                            inner_size_writer,
-                        } => {
-                            if !state.resize((
-                                (state.size().1 as f64 * scale_factor) as u32,
-                                state.size().1,
-                            )) {
-                                panic!("Couldn't resize!");
-                            }
-                            renderer.rescale_glyphs();
-                            redraw();
-                        }
-                        WindowEvent::ThemeChanged(_) => {}
-                        WindowEvent::Occluded(_) => {}
-                        WindowEvent::RedrawRequested => {
-                            // perform redraw
-                            redraw();
-                        }
-                        _ => {}
-                    }
-                }
-                Event::DeviceEvent { .. } => {}
-                Event::UserEvent(event) => {}
-                _ => {}
-            }
-        })
-        .unwrap();
+    iced::application("HCal", Calendar::update, Calendar::view)
+        .theme(Calendar::theme)
+        .run()
+        .unwrap()
 }
 
-#[derive(Clone)]
-struct DefaultScreen {
-    render: Arc<Container>,
+struct Calendar {
+    config: Config,
+    // the number of days offset from the current day
+    curr_view: isize,
+    rt: Runtime,
+    curr_render_event: Option<CalEvent>,
+    fetched_events: Vec<Vec<CalEvent>>,
 }
 
-impl DefaultScreen {
-
-    fn new() -> Self {
+impl Default for Calendar {
+    fn default() -> Self {
+        let cfg = Config::load();
+        let rt = Builder::new_current_thread()
+            .enable_time()
+            .enable_io()
+            .build()
+            .unwrap();
         Self {
-            render: Arc::new(Container::new()),
+            fetched_events: cfg
+                .calendars
+                .iter()
+                .map(|cal| cal.get_events(&rt).into_owned())
+                .collect::<Vec<_>>(),
+            config: cfg,
+            curr_view: 0,
+            rt,
+            curr_render_event: None,
+        }
+    }
+}
+
+impl Calendar {
+    fn theme(&self) -> Theme {
+        Theme::Dracula
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::RefreshCalendars => todo!(),
+            Message::MainScreen => {
+                self.curr_render_event = None;
+            }
+            Message::ViewEvent(cal_event) => {
+                self.curr_render_event = Some(cal_event);
+            }
         }
     }
 
+    fn view(&self) -> Element<Message> {
+        if let Some(event) = self.curr_render_event.as_ref() {
+            let start = chrono::DateTime::from_timestamp_millis(event.start as i64).unwrap();
+            let end = chrono::DateTime::from_timestamp_millis(event.finish as i64).unwrap();
+            let name = event.name.replace("\\, ", "\n");
+            let ev_text: Element<Message> = Text::new(format!(
+                "{} {}-{}\n{}\n{}",
+                wd_to_short_name(start.weekday()),
+                cut_off_end(&start.time().to_string(), 3),
+                cut_off_end(&end.time().to_string(), 3),
+                name,
+                &event.location
+            ))
+            .into();
+            let txt: Element<Message> = Text::new("Zurück").into();
+            let btn: Element<Message> =
+                Container::new(Button::new(txt).on_press(Message::MainScreen))
+                    .style(|theme| bordered_box(theme).border(Border::default().rounded(5)))
+                    .into();
+            return center(column![row![ev_text], row![btn]]).into();
+        }
+        println!("view!");
+        let mut days = {
+            let mut days = vec![];
+            for _ in 0..7 {
+                days.push(vec![]);
+            }
+            days
+        };
+        let now = Utc::now();
+        let mut earliest = u64::MAX;
+        let mut latest = 0;
+        let mut ev_iter = self.fetched_events.iter();
+        for cal in &self.config.calendars {
+            let events = ev_iter.next().unwrap();
+            for event in events {
+                println!(
+                    "got events: start {} ende {} name {} loc {} rep {:?}",
+                    event.start,
+                    event.finish,
+                    event.name,
+                    event.location,
+                    event.repeat /*reqwest::get(&event.name).unwrap().text().unwrap()*/
+                );
+                if event.start == 0 {
+                    continue;
+                }
+                if chrono::DateTime::from_timestamp_millis(event.start as i64)
+                    .unwrap()
+                    .checked_sub_days(Days::new(7))
+                    .unwrap()
+                    > chrono::Utc::now()
+                {
+                    continue;
+                }
+                if chrono::DateTime::from_timestamp_millis(event.finish as i64).unwrap()
+                    < chrono::Utc::now()
+                    && event.repeat.is_none()
+                {
+                    continue;
+                }
+                println!("curr millis: {}", Utc::now().timestamp_millis());
+                if let Some(rep) = event.repeat.as_ref() {
+                    match rep.freq.as_str() {
+                        "WEEKLY" => {
+                            // FIXME: support events spanning multiple days!
+                            let (new_start, days_off) = {
+                                let origin =
+                                    chrono::DateTime::from_timestamp_millis(event.start as i64)
+                                        .unwrap();
+                                let day = origin.weekday();
+                                let time = origin.time();
+                                if time.num_seconds_from_midnight() as u64 * 1000 < earliest {
+                                    earliest = time.num_seconds_from_midnight() as u64 * 1000;
+                                }
+                                let diff = Utc::now().weekday().number_from_monday();
+                                (
+                                    Utc::now()
+                                        .checked_sub_days(Days::new(diff as u64))
+                                        .unwrap()
+                                        .checked_add_days(Days::new(
+                                            day.num_days_from_monday() as u64
+                                        ))
+                                        .unwrap()
+                                        .with_time(time)
+                                        .unwrap(),
+                                    day.num_days_from_monday(),
+                                )
+                            };
+
+                            let new_end = {
+                                let origin =
+                                    chrono::DateTime::from_timestamp_millis(event.finish as i64)
+                                        .unwrap();
+                                let day = origin.weekday();
+                                let time = origin.time();
+                                if time.num_seconds_from_midnight() as u64 * 1000 > latest {
+                                    latest = time.num_seconds_from_midnight() as u64 * 1000;
+                                }
+                                let diff = Utc::now().weekday().number_from_monday();
+                                Utc::now()
+                                    .checked_sub_days(Days::new(diff as u64))
+                                    .unwrap()
+                                    .checked_add_days(Days::new(day.num_days_from_monday() as u64))
+                                    .unwrap()
+                                    .with_time(time)
+                                    .unwrap()
+                            };
+                            let mut new_ev = event.clone();
+                            new_ev.start = new_start.timestamp_millis() as u64;
+                            new_ev.finish = new_end.timestamp_millis() as u64;
+                            days[days_off as usize].push(new_ev);
+                        }
+                        _ => unimplemented!(),
+                    }
+                } else {
+                    let start =
+                        chrono::DateTime::from_timestamp_millis(event.start as i64).unwrap();
+                    let end = chrono::DateTime::from_timestamp_millis(event.finish as i64).unwrap();
+                    if start.time().num_seconds_from_midnight() as u64 * 1000 < earliest {
+                        earliest = start.time().num_seconds_from_midnight() as u64 * 1000;
+                    }
+                    if end.time().num_seconds_from_midnight() as u64 * 1000 > latest {
+                        latest = end.time().num_seconds_from_midnight() as u64 * 1000;
+                    }
+                    for i in 0..6 {
+                        if start
+                            .checked_add_days(Days::new(i))
+                            .unwrap()
+                            .date_naive()
+                            == now.date_naive()
+                        {
+                            days[i as usize].push(event.clone());
+                            println!("Got {i}: {}", event.name);
+                        }
+                    }
+                }
+            }
+        }
+
+        let mut days_row = Row::new();
+        let mut day_names = [
+            "Montag",
+            "Dienstag",
+            "Mittwoch",
+            "Donnerstag",
+            "Freitag",
+            "Samstag",
+            "Sonntag",
+        ]
+        .iter();
+
+        println!("midnight dist: {}", latest);
+
+        for events in days {
+            let day = day_names.next().unwrap();
+            let day_box = Container::new(Text::new(*day))
+                .width(Length::Fill)
+                .height(Length::Fixed(70.0));
+            let mut day_col = Column::new().push(day_box);
+            let mut last_event_end = 0;
+            let mut overall_space = 0;
+            for event in events {
+                println!("fixed up {} to {}", event.start, event.finish);
+                let start = chrono::DateTime::from_timestamp_millis(event.start as i64).unwrap();
+                let end = chrono::DateTime::from_timestamp_millis(event.finish as i64).unwrap();
+                // dirty fix for handling overlapping events
+                if last_event_end
+                    > (start.time().num_seconds_from_midnight() as u64 * 1000 - earliest)
+                {
+                    println!("skip!");
+                    continue;
+                }
+                let gap = (start.time().num_seconds_from_midnight() as u64 * 1000 - earliest)
+                    - last_event_end;
+                last_event_end = end.time().num_seconds_from_midnight() as u64 * 1000 - earliest;
+                println!("raw gap: {}", gap / 1000 / 60 / 10 * 9 * 2);
+                if gap > 0 {
+                    let gap_box =
+                        Container::new(Space::with_height(Length::Fill).width(Length::Fill))
+                            .height(Length::FillPortion((gap / 1000 / 60 / 10 * 9 * 2) as u16))
+                            .width(Length::Fill);
+                    day_col = day_col.push(gap_box);
+                }
+                // FIXME: support events that are happening at the same time (or overlapping)
+                let name = event.name.replace("\\, ", "\n");
+                day_col = day_col.push(
+                    Container::new(
+                        Button::new(Text::new(format!(
+                            "({}-{})\n{}\n{}",
+                            cut_off_end(&start.time().to_string(), 3),
+                            cut_off_end(&end.time().to_string(), 3),
+                            name,
+                            &event.location
+                        )))
+                        .on_press(Message::ViewEvent(event.clone()))
+                        .width(Length::Fill)
+                        .height(Length::Fill),
+                    )
+                    .width(Length::Fill)
+                    .height(Length::FillPortion(
+                        ((event.finish - event.start) / 1000 / 60 / 10 * 9 * 2) as u16,
+                    ))
+                    .style(|theme| {
+                        bordered_box(theme)
+                            .shadow({
+                                let mut shadow = Shadow::default();
+                                shadow.color = Color::BLACK;
+                                shadow
+                            })
+                            .border(Border::default().rounded(100))
+                    }),
+                );
+                println!(
+                    "raw day {}",
+                    (event.finish - event.start) / 1000 / 60 / 10 * 9 * 2
+                );
+                overall_space += (event.finish - event.start) / 1000 / 60 / 10 * 9 * 2;
+                overall_space += gap / 1000 / 60 / 10 * 9 * 2;
+            }
+            if last_event_end > 0 {
+                let day_off = (latest - earliest) - last_event_end;
+                println!("day off: {}", day_off / 1000 / 60 / 10 * 9 * 2);
+                if day_off > 0 {
+                    let gap_box =
+                        Container::new(Space::with_height(Length::Fill).width(Length::Fill))
+                            .height(Length::FillPortion(
+                                (day_off / 1000 / 60 / 10 * 9 * 2) as u16,
+                            ))
+                            .width(Length::Fill);
+                    day_col = day_col.push(gap_box);
+                    overall_space += day_off / 1000 / 60 / 10 * 9 * 2;
+                }
+            }
+            println!("overall used space: {}", overall_space);
+            days_row = days_row.push(day_col);
+        }
+
+        days_row.into()
+    }
 }
 
-impl Screen for DefaultScreen {
-    fn on_active(&mut self, _ctx: &Arc<screen_sys::AppCtx>) {
-        let glyph = ctx().renderer.add_glyph(GlyphInfo { in_bounds_off: (0.0, 0.0), size: (0.2, 0.2), text: "test".to_string(), attrs: AttrsOwned::new(Attrs::new()), shaping: Shaping::Basic, color: Color(u8::MAX as u32), scale: 1.0, x_offset: 0.0, y_offset: 0.0 });
-        self.render.add(Arc::new(RwLock::new(Box::new(TextBox {
-            pos: (0.0, 0.0),
-            width: 0.1,
-            height: 0.1,
-            coloring: [ui::Color {
-                r: 1.0,
-                g: 0.0,
-                b: 0.0,
-                a: 1.0,
-            }; 6],
-            texts: vec![glyph],
-        }))));
-    }
+fn cut_off_end(val: &str, cut: usize) -> &str {
+    &val[0..(val.len() - cut)]
+}
 
-    fn on_deactive(&mut self, _ctx: &Arc<screen_sys::AppCtx>) {
-        self.render.clear();
-    }
+#[derive(Debug, Clone)]
+enum Message {
+    RefreshCalendars,
+    MainScreen,
+    ViewEvent(CalEvent),
+}
 
-    fn tick(&mut self, _ctx: &Arc<screen_sys::AppCtx>) {
-        
-    }
+struct FetchedCalendar {
+    calendar: Calendar,
+    fetched_events: Vec<CalEvent>,
+}
 
-    fn container(&self) -> &Arc<ui::Container> {
-        &self.render
-    }
-
-    fn clone_screen(&self) -> Box<dyn Screen> {
-        Box::new(self.clone())
+fn wd_to_short_name(wd: Weekday) -> &'static str {
+    match wd {
+        Weekday::Mon => "Mo",
+        Weekday::Tue => "Di",
+        Weekday::Wed => "Mi",
+        Weekday::Thu => "Do",
+        Weekday::Fri => "Fr",
+        Weekday::Sat => "Sa",
+        Weekday::Sun => "So",
     }
 }
